56b08e865ee3e2fc463822b54a1d0be34c306e0b
Zeroid37 <smajomanovic@gmail.com>
2024-01-14 11:23:58 +0100

    Program fixed

---
+++ TicketVenueSystem/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketVenueSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
